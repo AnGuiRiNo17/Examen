f995ddb4666b33d5937cf68dbfadb5176c83d392
Your Name <you@example.com>
2024-10-17 09:53:25 -0500

    Cambio a Examen 2

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,9 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Add animation to nav items
-    const navItems = document.querySelectorAll('.nav-item');
+    const navItems = document.querySelectorAll ('.nav-item');
     navItems.forEach((item, index) => {
-        setTimeout(() => {
+        setTimeout (() => {
             item.classList.add('show');
         }, 100 * index);
-    });
-
-    // Update year in the footer
-    document.getElementById('year').textContent = new Date().getFullYear();
+    })
 });
+document.getElementById('year').textContent = new Date().getFullYear();
